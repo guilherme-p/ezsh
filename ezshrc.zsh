50a04c91407f3701d4747442c2c86c2d3a3029ab
Guilherme Peixoto <gpeixoto120@gmail.com>
2023-03-10 18:04:46 +0000

    changes

--- ezshrc.zsh
+++ ezshrc.zsh
@@ -59,7 +59,6 @@ ZSH_THEME="agnoster"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(zsh-completions zsh-autosuggestions zsh-syntax-highlighting history-substring-search k extract z sudo)
-    # 
     # httpie
     # git
     # python
